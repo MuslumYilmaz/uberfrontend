efb6d7311563b4f41f9194a30f2b19ff71e4fa58
MuslumYilmaz <mslmyilmaz34@gmail.com>
2025-07-27 22:21:10 +0300

    UI improvements & coding-detail route fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}"
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // replace the default sans
+        sans: ["Montserrat", "ui-sans-serif", "system-ui"],
+      },
+      colors: {
+        "brand-blue": "#3b82f6",
+        "brand-blue-dark": "#2563eb",
+      },
+    },
   },
   plugins: [],
-}
+};
