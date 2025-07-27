25240d396c3a75847e8231416ea8e5f0473b09fc
MuslumYilmaz <mslmyilmaz34@gmail.com>
2025-07-27 23:52:26 +0300

    trivia-details are added

--- src/app/features/trivia/trivia-detail/trivia-detail.component.ts
+++ src/app/features/trivia/trivia-detail/trivia-detail.component.ts
@@ -1,43 +1,76 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, signal } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  signal,
+} from '@angular/core';
+import {
+  ActivatedRoute,
+  RouterModule,
+} from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { Question } from '../../../core/models/question.model';
 import { QuestionService } from '../../../core/services/question.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-trivia-detail',
   standalone: true,
-  imports: [CommonModule, CardModule, ButtonModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    CardModule,
+    ButtonModule,
+  ],
   templateUrl: './trivia-detail.component.html',
-  styleUrls: ['./trivia-detail.component.scss']
+  styleUrls: ['./trivia-detail.component.scss'],
 })
-export class TriviaDetailComponent implements OnInit {
+export class TriviaDetailComponent implements OnInit, OnDestroy {
   tech!: string;
+
+  // full list of questions for this tech/trivia
+  questionsList: Question[] = [];
+
+  // currently selected question
   question = signal<Question | null>(null);
+
+  // drawer open state for answer
   showAnswer = signal(false);
+
+  private sub!: Subscription;
 
   constructor(
     private route: ActivatedRoute,
     private qs: QuestionService
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.route.paramMap.subscribe(params => {
-      const tech = params.get('tech') || 'javascript';
+    this.sub = this.route.paramMap.subscribe((params) => {
+      this.tech = params.get('tech') || 'javascript';
       const id = params.get('id')!;
-      this.tech = tech;
 
-      this.qs.loadQuestions(tech, 'trivia')
-        .subscribe(all => {
-          const q = all.find(q => q.id === id) ?? null;
-          this.question.set(q);
-        });
+      // load all trivia questions for this tech
+      this.qs.loadQuestions(this.tech, 'trivia').subscribe((all) => {
+        this.questionsList = all;
+        this.selectQuestion(id);
+      });
     });
   }
 
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  /** pick out and signal the current question by id */
+  private selectQuestion(id: string) {
+    const found = this.questionsList.find((q) => q.id === id) ?? null;
+    this.question.set(found);
+    this.showAnswer.set(false);
+  }
+
   toggleAnswer() {
-    this.showAnswer.update(v => !v);
+    this.showAnswer.update((v) => !v);
   }
 }
