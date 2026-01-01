2d2a6d396b50521073f1b6f7d6bbe03a6c863114
MuslumYilmaz <mslmyilmaz34@gmail.com>
2026-01-01 22:27:58 +0300

    vercel domain update

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: false,
     dataVersion: "2025-08-19a",
     apiBase: "/api", // 👈 proxy to http://localhost:3001/api
-    cdnBaseUrl: 'https://frontendatlas-six.vercel.app',
-    cdnEnabled: false,
+    cdnBaseUrl: 'https://frontendatlas.vercel.app',
+    cdnEnabled: true,
 };
